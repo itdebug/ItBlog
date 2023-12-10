28fed23d821c21e33972193f4466d477492c8d0a
Eric.Lu <17621698246@163.com>
2023-12-10 13:21:20 +0800

    编程哥 | 编程进阶之路

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,54 +1,103 @@
-import { defaultTheme } from '@vuepress/theme-default'
+import { defaultTheme } from "@vuepress/theme-default";
 
 export default {
-    title: '编程哥 | 编程进阶之路',
-    description: '记录学习过程，总结学习经验',
-    head: [
-        ['link', { rel: 'icon', href: '/images/logo.jpg' }]
-    ],
-    dest: ".vuepress/dist",
-    theme: defaultTheme({
-        navbar: [
-            // NavbarItem
-            {
-                text: 'Java',
-                link: '/',
-            },
-            // NavbarGroup
-            {
-                text: 'Spring',
-                children: [
-                    {
-                        text: 'Spring 手撸专栏',
-                        link: '/md/spring/develop-spring/001.md'
-                        // link: 'https://mp.weixin.qq.com/s/olrwapkSTQMyIGpR10ZDzA'
-                    }],
-            },
-            {
-                text: '部署',
-                link: '/',
-            },
+  lang: "zh-CN",
+  title: "编程哥 | 编程进阶之路",
+  description: "记录学习过程，总结学习经验",
+  head: [["link", { rel: "icon", href: "/images/logo.jpg" }]],
+  //打包目录地址，并且会在git提交中忽略上传
+  dest: ".vuepress/dist",
+  theme: defaultTheme({
+    navbar: [
+      // NavbarItem
+      {
+        text: "学习路线",
+        link: "/md/study-map/study-map.md",
+      },
+      {
+        text: "Java",
+        link: "/md/java/java基础.md",
+      },
+      // NavbarGroup
+      {
+        text: "Spring",
+        children: [
+          {
+            text: "Spring 手撸专栏",
+            link: "/md/spring/develop-spring/001.md",
+            // link: 'https://mp.weixin.qq.com/s/olrwapkSTQMyIGpR10ZDzA'
+          },
         ],
-        sidebar: {
-            '/md/spring/develop-spring/': [
-                {
-                    text: '介绍',
-                    collapsible: false,
-                    children: [{
-                        text: 'Spring 手撸专栏',
-                        link: '/md/spring/develop-spring/002.md'
-                    }],
-                },
+      },
+      {
+        text: "部署",
+        link: "/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/itdebug",
+      },
+    ],
+    sidebar: {
+      "/md/study-map/": getBarStudyMap(),
+      "/md/java/": getBarJava(),
+      "/md/spring/develop-spring/": getBarSpring(),
+    },
+  }),
+};
 
-                {
-                    text: '计划',
-                    collapsible: false,
-                    children: [{
-                        text: '2023计划',
-                        link: '/md/spring/develop-spring/003.md'
-                    }],
-                },
-            ],
+function getBarStudyMap() {
+  return [
+    {
+      text: "博客搭建",
+      collapsible: false,
+      children: [
+        {
+          text: "Vuepress",
+          link: "/md/study-map/vuepress.md",
         },
-    })
+      ],
+    },
+  ];
+}
+
+function getBarJava() {
+  return [
+    {
+      text: "Java基础",
+      collapsible: false,
+      children: [
+        {
+          text: "基础语法",
+          link: "/md/java/basic/java-basic.md",
+        },
+      ],
+    },
+  ];
+}
+
+function getBarSpring() {
+  return [
+    {
+      text: "介绍",
+      collapsible: false,
+      children: [
+        {
+          text: "Spring 手撸专栏",
+          link: "/md/spring/develop-spring/002.md",
+        },
+      ],
+    },
+
+    {
+      text: "计划",
+      collapsible: false,
+      children: [
+        {
+          text: "2023计划",
+          link: "/md/spring/develop-spring/003.md",
+        },
+      ],
+    },
+  ];
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
----   
+---
 home: true
 heroImage: /images/home.png
 heroHeight: 200
@@ -9,15 +9,15 @@ actions:
     link: /md/other/guide-to-reading.md
     type: primary
   - text: 学习路线
-    link: /md/other/road-map.md
+    link: /md/study-map/study-map.md
     type: secondary
 features:
-- title: 闻鸡起舞
-  details: 三更灯火五更鸡，正是男儿读书时
-- title: 一往无前
-  details: 一日不读书，胸臆无佳想
-- title: 坚持不懈
-  details: 一月不读书，耳目失精爽
+  - title: 闻鸡起舞
+    details: 三更灯火五更鸡，正是男儿读书时
+  - title: 一往无前
+    details: 一日不读书，胸臆无佳想
+  - title: 坚持不懈
+    details: 一月不读书，耳目失精爽
 footer: <a href="http://beian.miit.gov.cn" target="_blank">沪ICP备2023032798号-1</a> | <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=xxx" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="/images/beian.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin-top:5px; color:#939393;"></p></a> | Copyright © 2023 All rights reserved.
 footerHtml: true
 ---
